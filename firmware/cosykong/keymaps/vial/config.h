2c134426199ce874d54d5596074a7a61c3a8fa67
weteor <github@wetreg.fastmail.net>
2023-07-25 15:45:09 +0200

    add firmware

---
+++ firmware/cosykong/keymaps/vial/config.h
@@ -0,0 +1,25 @@
+/*
+Copyright 2022 weteor
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x5C, 0x53, 0xC7, 0xC5, 0x34, 0xCF, 0xD1, 0xF8}
+
+#define VIAL_KEY_OVERRIDE_ENTRIES 20
+#define VIAL_COMBO_ENTRIES 20
+#define VIAL_TAP_DANCE_ENTRIES 20
+#define DYNAMIC_KEYMAP_LAYER_COUNT 6
---
+++ firmware/cosykong/readme.md
@@ -0,0 +1,1 @@
+## cos(y) Kong
